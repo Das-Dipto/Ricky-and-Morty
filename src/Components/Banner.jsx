adc4ef49fff5b4c3937bafbb27cd63c302641559
Dipto Das <ddipto396@gmail.com>
2023-06-26 00:22:41 +0600

    watch-now button and message added in banner-content

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -4,6 +4,7 @@ import pill from '../assets/Media/HomePage/HeroElements/pill.png'
 import portal from '../assets/Media/HomePage/HeroElements/portal.png'
 import bubble from '../assets/Media/HomePage/HeroElements/bubble.png'
 import gun from '../assets/Media/HomePage/HeroElements/Gun.png'
+import {RiPlayCircleLine} from 'react-icons/ri';
 
 const Banner = () => {
   return (
@@ -17,8 +18,8 @@ const Banner = () => {
               <div className="heading-text">
                 <div className='relative'>
                 <h1 className='banner-heading-text text-white flex text-3xl md:text-8xl font-bold'> 
-                  <span className='italic me-3 relative'>THE <img className='absolute -top-7 right-7 md:-top-24 md:right-24' src={bubble} alt="" /> </span> 
-                  <img className=' w-[50px] md:w-[15%] relative -top-3 md:-top-5' src={portal} alt="" /> 
+                  <span className='italic me-3 relative'>THE <img className='absolute -top-7 right-7 md:-top-24 md:right-24' src={bubble} alt="bubble" /> </span> 
+                  <img className=' w-[50px] md:w-[15%] relative -top-3 md:-top-5' src={portal} alt="portal" /> 
                   <span className='ms-3 medium-green'>RIC<span className='medium-blue me-5'>K</span> 
                   <span className='medium-blue relative'>& <img className=' hidden md:flex absolute left-10 -top-4' src={pill} alt="pill" /></span>  </span> 
                  
@@ -27,16 +28,18 @@ const Banner = () => {
                 <div className="relative">
                   <h1 className='banner-heading-text text-white text-3xl md:text-8xl font-bold mt-5'> 
                   <span className='medium-green'>MORTY</span> <span className='italic'>WIKI
-                  <img className='absolute -right-2 md:-right-20 -top-2 md:-top-10 w-[30%]' src={gun} alt="pill" />
+                  <img className='absolute -right-2 md:-right-20 -top-2 md:-top-10 w-[30%]' src={gun} alt="gun" />
                   </span> 
                   </h1> 
                 </div>
               </div>
            </div>
+
+            <div className=' mt-7 button-&-message flex flex-row flex-wrap-reverse justify-center align-center'>
+               <button className="btn watch-now-button text-white font-semibold mt-7 md:mt-0 me-10"> <RiPlayCircleLine style={{fontSize:'20'}}/> Watch Now</button>
+               <p className='medium-blue mt-8 w-[90%] mx-auto md:w-[25%] md:mx-0 message'>Brilliant but boozy scientist Rick hijacks his fretful teenage grandson, Morty, for wild escapades in other worlds and alternate dimensions</p>
+            </div>
                         
-            {/* <div className='button-&-message'>
-               <button class="btn btn-outline">Default</button>
-            </div> */}
         </div>
       </div>
     </>
